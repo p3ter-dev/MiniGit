a78f125f5bbd490669db513a32653a0a8f59e9b2
peter <peterkinfe548@gmail.com>
2025-06-21 12:44:01 +0300

    updated main file

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "MiniGit.h"
+
+int main() {
+    MiniGit repo;
+    repo.init();
+    return 0;
+}
+// This is the main entry point for the MiniGit application.
+// It initializes a new MiniGit repository by creating the necessary directories and files.
