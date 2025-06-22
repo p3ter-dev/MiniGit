54d039ddd5421603303e7e8f058b10d7529fc6c6
peter <peterkinfe548@gmail.com>
2025-06-22 13:57:44 +0300

    header file for FileIO

--- src/utils/FileIO.h
+++ src/utils/FileIO.h
@@ -1,4 +1,7 @@
 #pragma once
 #include <string>
+#include <vector>
 
 std::string readFileContent(const std::string& filePath);
+std::vector<std::string> readStagedFiles();
+std::string readCurrentHead();// Reads the current HEAD commit hash (from .minigit/refs/main)
