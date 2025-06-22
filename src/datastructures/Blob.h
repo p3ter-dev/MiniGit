29bef71e956728ead179a5862487af8713b2703e
peter <peterkinfe548@gmail.com>
2025-06-22 12:27:00 +0300

    added header file to blob

--- src/datastructures/Blob.h
+++ src/datastructures/Blob.h
@@ -1,29 +1,11 @@
-#ifndef BLOB_H
-#define BLOB_H
-
+#pragma once
 #include <string>
 
 class Blob {
 public:
-    // Constructor: takes in the file path, reads it and computes hash
-    Blob(const std::string& filename);
-
-    // Save content to blob file (.minigit/objects/<hash>)
-    void writeToObjectStore() const;
-
-    // Getter for hash (used in indexing, commits)
-    std::string getHash() const;
-
-    // Getter for file name (optional use)
-    std::string getFilename() const;
-
-private:
     std::string filename;
-    std::string content;
     std::string hash;
 
-    // Helper to load content and hash
-    void readAndHashFile();
+    Blob(const std::string& filename, const std::string& hash);
+    void save(const std::string& content) const;
 };
-
-#endif
