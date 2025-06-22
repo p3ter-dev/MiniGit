4835a754adb9f658be75e65f8d90f0ef0e8e4f93
peter <peterkinfe548@gmail.com>
2025-06-22 13:57:08 +0300

    implemented reading staged files and current head

--- src/utils/FileIO.cpp
+++ src/utils/FileIO.cpp
@@ -1,4 +1,6 @@
 #include "./FileIO.h"
+#include <vector>
+#include <string>
 #include <fstream>
 #include <sstream>
 
@@ -9,4 +11,22 @@ std::string readFileContent(const std::string& filePath) {
     std::ostringstream ss;
     ss << inFile.rdbuf();
     return ss.str();
+}
+
+std::vector<std::string> readStagedFiles() {
+    std::ifstream stage(".minigit/stage");
+    std::vector<std::string> files;
+    std::string line;
+    while (std::getline(stage, line)) {
+        if (!line.empty())
+            files.push_back(line);
+    }
+    return files;
+}
+
+std::string readCurrentHead() {
+    std::ifstream refFile(".minigit/refs/main");
+    std::string headHash;
+    std::getline(refFile, headHash);
+    return headHash.empty() ? "null" : headHash;
 }
