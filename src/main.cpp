165445e83019d83e504c49c18b16b07bff391938
peter <peterkinfe548@gmail.com>
2025-06-22 13:52:16 +0300

    updated main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "MiniGit.h"
 #include "./commands/add.h"
+#include "./commands/commit.h"
+
 int main() {
     MiniGit repo;
     repo.init();
     addFileToStage("test.txt"); // test file to stage
+    commit("Initial commit"); // commit staged file
     return 0;
 }
 // This is the main entry point for the MiniGit application.
-// It initializes a new MiniGit repository by creating the necessary directories and files.
