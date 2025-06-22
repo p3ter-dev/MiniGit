b4ed42321fb8f73748871f46f0dbd6401f459c8b
EbenezerD77 <abenezerdyirga@gmail.com>
2025-06-22 16:42:49 +0300

    Updated the main.cpp file to include showLog method.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include "MiniGit.h"
-#include "../commands/log.h"
+#include "./commands/log.h"
 #include "./commands/add.h"
 #include "./commands/commit.h"
+using command::showLog;
 
 int main() {
     MiniGit repo;
     repo.init();
     addFileToStage("test.txt"); // test file to stage
     commit("Initial commit"); // commit staged file
-    showLog();
+    showLog(); // display commit log
     return 0;
 }
 // This is the main entry point for the MiniGit application.
