eda7d1f7010fcaa95b1f84643801601627d136ff
peter <peterkinfe548@gmail.com>
2025-06-22 20:15:21 +0300

    updated main function

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,16 @@
 #include "./commands/log.h"
 #include "./commands/add.h"
 #include "./commands/commit.h"
+#include "./datastructures/branch.h"
 using command::showLog;
 
 int main() {
-    MiniGit repo;
-    repo.init();
-    addFileToStage("test.txt"); // test file to stage
-    commit("Initial commit"); // commit staged file
-    showLog(); // display commit log
+    // MiniGit repo;
+    // repo.init();
+    // addFileToStage("test.txt"); // test file to stage
+    // commit("Initial commit"); // commit staged file
+    // showLog(); // display commit log
+    createBranch("feature-something"); // create a new branch
     return 0;
 }
 // This is the main entry point for the MiniGit application.
