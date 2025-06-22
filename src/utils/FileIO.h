59a99f3b22ef85b5b88497d8f139c965857046d1
peter <peterkinfe548@gmail.com>
2025-06-22 12:29:26 +0300

    added header file reader IO

--- src/utils/FileIO.h
+++ src/utils/FileIO.h
@@ -1,17 +1,4 @@
-#ifndef FILE_IO_H
-#define FILE_IO_H
-
+#pragma once
 #include <string>
-#include <unordered_map>
 
-namespace utils {
-
-    std::string readFile(const std::string& filename);
-    void writeFile(const std::string& path, const std::string& content);
-
-    std::unordered_map<std::string, std::string> readIndex(const std::string& indexPath);
-    void writeIndex(const std::string& indexPath, const std::unordered_map<std::string, std::string>& indexMap);
-
-}
-
-#endif
+std::string readFileContent(const std::string& filePath);
