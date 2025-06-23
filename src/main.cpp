0944943a5dd10b11d31054266c0677e637f4157f
Kirubelyitagesu <kirubelyitagesu16@gmail.com>
2025-06-23 01:56:18 -0700

    Call basic checkout from main to test integration

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "./commands/add.h"
 #include "./commands/commit.h"
 #include "./datastructures/branch.h"
+#include "./commands/checkout.h"
 using command::showLog;
 
 int main() {
@@ -12,6 +13,7 @@ int main() {
     commit("Initial commit"); // commit staged file
     showLog(); // display commit log
     createBranch("feature-something"); // create a new branch
+    checkout("test-branch"); // Testing basic checkout print
     return 0;
 }
 // This is the main entry point for the MiniGit application.
