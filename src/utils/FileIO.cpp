7f57b2443d9810510b6815b366c20328c6f5ab96
peter <peterkinfe548@gmail.com>
2025-06-23 13:35:47 +0300

    updated reading parent commit hash from the commit file

--- src/utils/FileIO.cpp
+++ src/utils/FileIO.cpp
@@ -30,3 +30,13 @@ std::string readCurrentHead() {
     std::getline(refFile, headHash);
     return headHash.empty() ? "null" : headHash;
 }
+
+std::string readParentHash(const std::string& commitHash) {
+    std::ifstream file(".minigit/commits/" + commitHash);
+    std::string line;
+    while (std::getline(file, line)) {
+        if (line.rfind("parent: ", 0) == 0)
+            return line.substr(8);
+    }
+    return "null";
+}
